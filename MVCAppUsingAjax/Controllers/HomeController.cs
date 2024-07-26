d06469d1dc8793a9344cd0eb8dff9bbf396d4c83
My Challenge <agilemahesh33@gmail.com>
2024-07-26 22:59:39 +0530

    Add project files.

---
+++ MVCAppUsingAjax/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Xml.Linq; //For using LINQ to XML
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public String GetScore()
+        {
+            //Gets the physical path of the file for the given virtual path
+            string physicalPath = Server.MapPath("~/Matches/Score.xml");
+            //Loads the XML file into the application from the specified location
+            var doc = XElement.Load(physicalPath);
+            //Reads the value or content of "Score" element from the xml file which is loaded.
+            string score = doc.Element("Score").Value;
+            return score;
+
+            //Above code can be written using single line 
+            //XElement.Load(Server.MapPath("~/Matches/Score.xml")).Element("Score").Value;
+        }
+    }
+}
